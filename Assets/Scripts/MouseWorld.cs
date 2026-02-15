ec4d043d28b775a50a724729a381eb6706c7d2f8
Elden Modder <vancoppenollebrendan@gmail.com>
2026-02-15 15:53:27 +0100

    First commit + Finish Part 1

---
+++ Assets/Scripts/MouseWorld.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+public class MouseWorld : MonoBehaviour
+{
+    public static MouseWorld instance;
+    [SerializeField] private LayerMask mousePlaneLayerMask;
+    private void Awake()
+    {
+        instance = this;
+    }
+    void Update()
+    {
+        transform.position = MouseWorld.GetPosition();
+    }
+
+    public static Vector3 GetPosition()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        Physics.Raycast(ray, out RaycastHit hit, float.MaxValue, instance.mousePlaneLayerMask);
+        return hit.point;
+    }
+}
---
+++ Assets/Scripts/Unit.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+
+public class Unit : MonoBehaviour
+{
+    private Vector3 targetPosition;
+    private Animator unitAnimator;
+
+    private void Start()
+    {
+        unitAnimator = GetComponentInChildren<Animator>();
+        targetPosition = transform.position;
+    }
+
+    private void Update()
+    {
+        float stoppingDistance = .1f;
+        if (Vector3.Distance(transform.position, targetPosition) > stoppingDistance)
+        {
+            Vector3 moveDirection = (targetPosition - transform.position).normalized;
+            float moveSpeed = 4f;
+            transform.position += moveDirection * moveSpeed * Time.deltaTime;
+
+            float rotateSpeed = 10f;
+            transform.forward = Vector3.Lerp(
+                transform.forward, 
+                moveDirection, 
+                Time.deltaTime * rotateSpeed
+            ); //smoothly rotate when moving
+
+            unitAnimator.SetBool("IsWalking", true);
+        }
+        else unitAnimator.SetBool("IsWalking", false);
+    }
+    public void Move(Vector3 targetPosition) => this.targetPosition = targetPosition;
+}
